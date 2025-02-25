1c3f07a505cfbeb0c4237dea7ed62d9d0e628ff0
anika-moumeta <anikaafrin3344@gmail.com>
2025-02-25 12:31:56 +0600

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_5/body.dart';
+
 void main() {
   runApp(const Lab06());
 }
@@ -11,14 +13,6 @@ class Lab06 extends StatelessWidget {
     return MaterialApp(
       title: 'Advanced UI',
       home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.power), label: "Power"),
-          ],
-        ),
         drawer: Drawer(
           child: ListView(
             children: [
@@ -36,8 +30,8 @@ class Lab06 extends StatelessWidget {
                       child: Icon(Icons.closed_caption_rounded),
                     ),
                   ],
-                  ),
-                  ),
+                ),
+              ),
               ListTile(
                 leading: Icon(Icons.people_alt_rounded),
                 title: Text("Contacts"),
@@ -49,22 +43,12 @@ class Lab06 extends StatelessWidget {
                 onTap: () {},
               ),
             ],
-          )),
-          appBar: AppBar(
-            title: Text("Advance UI"),
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.network(
-                    "https://images.unsplash.com/photo-1635805737707-575885ab0820?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-                Image.network(
-                    "https://images.unsplash.com/photo-1635805739892-ab7b431400f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-                Image.network(
-                    "https://images.unsplash.com/photo-1732445027511-4a6cde297c31?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D"),
-              ],
-            ),
-          ),
+        ),
+        appBar: AppBar(
+          title: Text("Advance UI"),
+        ),
+        body: AppBody(),
       ),
     );
   }
